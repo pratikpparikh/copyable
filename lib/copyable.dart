4c2af35ce458a29aa4eed6ab7b6ad071d013cd63
Pratik Parikh <pratik.p.parikh@gmail.com>
2020-03-02 08:42:01 -0500

    'removedflutter'

--- lib/copyable.dart
+++ lib/copyable.dart
@@ -1,5 +1,3 @@
 library copyable;
 
 export 'package:copyable/src/copyable.dart';
-
-export 'package:copyable/src/builtins/builtins.dart';
--- test/copyable_test.dart
+++ test/copyable_test.dart
@@ -1,7 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:copyable/copyable.dart';
-
 void main() {
   // TODO: Tests
 }
